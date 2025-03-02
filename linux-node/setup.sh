754560b9b16cf9c535af08b8ef9ef18cf1957bf2
Alexander Mauri <maurialex@web.de>
2025-03-02 14:07:09 +0000

    swap off

--- linux-node/setup.sh
+++ linux-node/setup.sh
@@ -5,7 +5,19 @@ echo "Root folder: $rootFolder"
 
 source "$rootFolder/../.env"
 
-"$rootFolder/setup-cni.sh"
-"$rootFolder/setup-runc.sh"
-"$rootFolder/setup-crictl.sh"
-"$rootFolder/setup-containerd.sh"
+# "$rootFolder/setup-cni.sh"
+# "$rootFolder/setup-runc.sh"
+# "$rootFolder/setup-crictl.sh"
+# "$rootFolder/setup-containerd.sh"
+
+# sudo apt install -y jq yq
+
+sudo swapoff --all
+
+cat "/etc/fstab" | \
+jq --raw-input \
+'
+(select(test("^[^#].*\\sswap\\s"))) |= ("# "+.)
+' \
+--raw-output | sudo tee "/etc/fstab" > /dev/null
+
