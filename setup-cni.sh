2f75fef881e8916a2f8bb43fa8398a1e2cf74465
Alexander Mauri <maurialex@web.de>
2025-03-29 06:14:11 +0000

    cni tools

--- setup-cni.sh
+++ setup-cni.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 rootFolder=$(dirname "$BASH_SOURCE")
-source "$rootFolder/.env"
+. "$rootFolder/.env"
 
 tempOutputFolder=$(mktemp --directory)
 echo "Temporary output folder: $tempOutputFolder"
 
-wget https://github.com/containernetworking/plugins/releases/download/$CNI_VERSION/cni-plugins-linux-amd64-$CNI_VERSION.tgz \
+# https://github.com/containernetworking/plugins/releases
+wget https://github.com/containernetworking/plugins/releases/download/v$CNI_VERSION/cni-plugins-linux-amd64-v$CNI_VERSION.tgz \
 --output-document="$tempOutputFolder/cni-plugins.tgz"
 
-sudo mkdir --parents /opt/cni/bin
-sudo tar --extract --file="$tempOutputFolder/cni-plugins.tgz" --directory=/opt/cni/bin
+sudo mkdir --parents "$CNI_BIN_PATH"
+sudo tar --extract --file="$tempOutputFolder/cni-plugins.tgz" --directory="$CNI_BIN_PATH"
 
 rm --recursive "$tempOutputFolder"
